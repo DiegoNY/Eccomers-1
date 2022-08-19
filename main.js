7061be793e975e3013420fc45468c551cde933ff
quinua_1 <nalvartediego40@gmail.com>
2022-08-19 11:06:42 -0500

    Uniendo el menu mobile a la pagina uwu

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //declaramos variables
 const menu = document.querySelector('.navbar-email');
 const menuDestok = document.querySelector('.desktop-menu');
-
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 //escuchamos funcion click para mostrar menu
 menu.addEventListener('click',toggleDestokMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 function toggleDestokMenu(){
     menuDestok.classList.toggle('inactive');
 }
 
-//
-console.log('Scrip funcionando');
+function toggleMobileMenu(){
+
+}
