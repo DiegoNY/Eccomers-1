1a05c7f1e707fd2ea61964880689d12d54de37ee
quinua_1 <nalvartediego40@gmail.com>
2022-08-22 21:45:40 -0500

    Agregando nuevos estilos al carrito y funcionalidad responsive a la page principal uwu

--- main.js
+++ main.js
@@ -3,12 +3,32 @@ const menu = document.querySelector('.navbar-email');
 const menuDestok = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
+const carrito = document.querySelector('.navbar-shopping-cart');
+const conteinCar =document.querySelector('.car');
 //escuchamos funcion click para mostrar menu
 menu.addEventListener('click',toggleDestokMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+carrito.addEventListener('click',toggleCarrito);
+
+function toggleCarrito(){
+    // const menuDesac = menuDestok.classList.contains('inactive');
+    const menuMobDes = menuMobile.classList.contains('inactive');
+    if(menuMobDes ){
+        conteinCar.classList.toggle('inactive');
+    
+    }else{
+        
+         menuMobile.classList.add('inactive');
+    }
+}
 
 function toggleDestokMenu(){
+    const carDesac = conteinCar.classList.contains('inactive');
+    if(carDesac){
     menuDestok.classList.toggle('inactive');
+    }else{
+        conteinCar.classList.add('inactive');
+    }
 }
 
 function toggleMobileMenu(){
