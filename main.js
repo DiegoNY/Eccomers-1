1ba44295dc5be8e41a1cc9c49af4871e8f7f769c
quinua_1 <nalvartediego40@gmail.com>
2022-08-23 19:06:42 -0500

    Agregando funcionalidad de carrito y productos en el index

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
 //declaramos variables
+
 const menu = document.querySelector('.navbar-email');
 const menuDestok = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 const carrito = document.querySelector('.navbar-shopping-cart');
 const conteinCar =document.querySelector('.car');
+
 //escuchamos funcion click para mostrar menu
 menu.addEventListener('click',toggleDestokMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 carrito.addEventListener('click',toggleCarrito);
 
 function toggleCarrito(){
-    // const menuDesac = menuDestok.classList.contains('inactive');
-    const menuMobDes = menuMobile.classList.contains('inactive');
-    if(menuMobDes ){
-        conteinCar.classList.toggle('inactive');
     
-    }else{
-        
-         menuMobile.classList.add('inactive');
+    const isMenuMobilClosed = menuMobile.classList.contains('inactive');
+    //menuDestok Inactivo
+    const isMenuDestockClosed = menuDestok.classList.contains('inactive');
+
+    if(!isMenuMobilClosed){
+        menuMobile.classList.add('inactive');
     }
+    conteinCar.classList.toggle('inactive');
+   
 }
 
 function toggleDestokMenu(){
-    const carDesac = conteinCar.classList.contains('inactive');
-    if(carDesac){
-    menuDestok.classList.toggle('inactive');
-    }else{
+    const isCarConteinClosed = conteinCar.classList.contains('inactive');
+    if(!isCarConteinClosed){
         conteinCar.classList.add('inactive');
     }
+    menuDestok.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isCarClosed = conteinCar.classList.contains('inactive');
+
+    if(!isCarClosed){
+        conteinCar.classList.add('inactive');
+    }
     menuMobile.classList.toggle('inactive');
 }
 
